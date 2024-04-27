bfd3271d21f2ef8b864fae9e8255e781d998a69e
Ruan C. Keet <rckeet@gmail.com>
2024-04-27 22:07:03 +0200

    reformat

--- src/debug.h
+++ src/debug.h
@@ -2,13 +2,13 @@
 #define DEBUG_H
 
 #ifndef NDEBUG
+#include "instructions.h"
+
 #include <stdint.h>
 #include <stddef.h>
 
 #define DISASSEMBLE(rom_name, prog, size) disassemble(rom_name, prog, size)
 #define DEBUG_OPCODE(op) debug_opcode(op)
-
-#include "instructions.h"
 
 void disassemble(const char *rom_name, uint8_t *prog, size_t size);
 void debug_opcode(opcode_t *op);
