e638f455a8e4502397e00eec8ab06c3def41a462
Ruan C. Keet <rckeet@gmail.com>
2025-07-16 17:50:31 +0200

    Added windows specific main for release mode

--- src/main.c
+++ src/main.c
@@ -1,5 +1,29 @@
 #include "client/client.h"
+#include "core/platform.h"
 
+#if defined(C8_PLATFORM_WINDOWS) && defined(C8_RELEASE)
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+    (void)hInstance;     // --+
+    (void)hPrevInstance; //   |
+    (void)lpCmdLine;     //   +-- unused parameters
+    (void)nShowCmd;      // --+
+
+    C8Client *const client = c8InitClient(__argc, __argv);
+    if (!client)
+    {
+        return C8_FAILURE;
+    }
+
+    c8RunClient(client);
+    c8CloseClient(client);
+
+    return C8_SUCCESS;
+}
+
+#else
 int main(int argc, char **argv)
 {
     C8Client *const client = c8InitClient(argc, argv);
@@ -13,3 +37,4 @@ int main(int argc, char **argv)
 
     return C8_SUCCESS;
 }
+#endif
